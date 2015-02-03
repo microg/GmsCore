19b0b0f1d782640c0e5a9a381a0191665de0b053
mar-v-in <github@rvin.mooo.com>
2015-02-03 23:07:55 +0100

    Add SHA1 to Constants

--- src/org/microg/gms/common/Constants.java
+++ src/org/microg/gms/common/Constants.java
@@ -10,6 +10,7 @@ public class Constants {
     public static final String KEY_MOCK_LOCATION = "mockLocation";
     public static final String DEFAULT_ACCOUNT = "<<default account>>";
     public static final String GMS_PACKAGE_NAME = "com.google.android.gms";
+    public static final String GMS_PACKAGE_SIGNATURE_SHA1 = "38918a453d07199354f8b19af05ec6562ced5788";
 
     /**
      * No base map tiles.
