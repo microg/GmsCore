adb160c25bf23be73738a732c94b903db41a338d
mar-v-in <github@rvin.mooo.com>
2015-03-04 23:22:49 +0100

    Beginning IPeopleService

--- src/com/google/android/gms/common/data/DataHolder.java
+++ src/com/google/android/gms/common/data/DataHolder.java
@@ -1,11 +1,11 @@
 package com.google.android.gms.common.data;
 
 import org.microg.safeparcel.AutoSafeParcelable;
+import org.microg.safeparcel.SafeParceled;
 
-/**
- * TODO: usage
- */
 public class DataHolder extends AutoSafeParcelable {
+    @SafeParceled(1000)
+    private int versionCode = 1;
 
     public static final Creator<DataHolder> CREATOR = new AutoCreator<>(DataHolder.class);
 }
--- src/com/google/android/gms/people/internal/IPeopleCallbacks.aidl
+++ src/com/google/android/gms/people/internal/IPeopleCallbacks.aidl
@@ -1,4 +1,7 @@
 package com.google.android.gms.people.internal;
 
+import com.google.android.gms.common.data.DataHolder;
+
 interface IPeopleCallbacks {
+    void onDataHolders(int code, in Bundle meta, in DataHolder[] data) = 3;
 }
