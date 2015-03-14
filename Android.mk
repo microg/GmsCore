c0c4a78eab3064cdd7545cb7fcdddfe93ca1674d
mar-v-in <github@rvin.mooo.com>
2015-03-14 22:09:18 +0100

    Update safe-parcel

--- Android.mk
+++ Android.mk
@@ -3,8 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := GmsApi
-LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
-LOCAL_SRC_FILES := $(call all-Iaidl-files-under, src/main/aidl)
+LOCAL_SRC_FILES := $(call all-java-files-under, play-services-api/src/main/java)
+LOCAL_SRC_FILES := $(call all-Iaidl-files-under, play-services-api/src/main/aidl)
 LOCAL_STATIC_JAVA_LIBRARIES := SafeParcel
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
