c6a81f4d7d978418352648cecd2545ea575a8caf
Marvin W <git@larma.de>
2016-02-11 18:13:10 +0100

    Add references to resources and AndroidManifest.xml to Android.mk
    related to microg/android_packages_apps_UnifiedNlp#57

--- Android.mk
+++ Android.mk
@@ -3,7 +3,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := MicroGUiTools
 LOCAL_SRC_FILES := $(call all-java-files-under, microg-ui-tools/src/main/java)
-
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/microg-ui-tools/src/main/res
+LOCAL_MANIFEST_FILE := $(LOCAL_PATH)/microg-ui-tools/src/main/AndroidManifest.xml
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v7-appcompat
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
