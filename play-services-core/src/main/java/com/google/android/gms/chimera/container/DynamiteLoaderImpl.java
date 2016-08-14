5a0c235e3ddd2e5d28ab1ca5b4848f3a9e5c3065
Marvin W <git@larma.de>
2016-08-14 15:03:22 +0200

    Add dummy support for firebase database API

---
+++ play-services-core/src/main/java/com/google/android/gms/chimera/container/DynamiteLoaderImpl.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2013-2016 microG Project Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.gms.chimera.container;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.pm.PackageManager;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.google.android.gms.dynamic.IObjectWrapper;
+import com.google.android.gms.dynamic.ObjectWrapper;
+import com.google.android.gms.dynamite.IDynamiteLoader;
+
+import org.microg.gms.common.Constants;
+
+public class DynamiteLoaderImpl extends IDynamiteLoader.Stub {
+    private static final String TAG = "GmsDynamiteLoaderImpl";
+
+    @Override
+    public IObjectWrapper createModuleContext(IObjectWrapper wrappedContext, String moduleId, int minVersion) throws RemoteException {
+        Log.d(TAG, "unimplemented Method: createModuleContext for " + moduleId + " at version " + minVersion + ", returning gms context");
+        final Context context = (Context) ObjectWrapper.unwrap(wrappedContext);
+        try {
+            return ObjectWrapper.wrap(new ContextWrapper(context.createPackageContext(Constants.GMS_PACKAGE_NAME, Context.CONTEXT_INCLUDE_CODE | Context.CONTEXT_IGNORE_SECURITY)) {
+                @Override
+                public Context getApplicationContext() {
+                    return context;
+                }
+            });
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.w(TAG, "returning null instead", e);
+            return null;
+        }
+    }
+
+    @Override
+    public int getModuleVersion(IObjectWrapper context, String moduleId) throws RemoteException {
+        return getModuleVersion2(context, moduleId, true);
+    }
+
+    @Override
+    public int getModuleVersion2(IObjectWrapper context, String moduleId, boolean updateConfigIfRequired) throws RemoteException {
+        if (moduleId.equals("com.google.android.gms.firebase_database")) {
+            Log.d(TAG, "returning temp fix module version for " + moduleId + ". Firebase Database will not be functional!");
+            return com.google.android.gms.dynamite.descriptors.com.google.android.gms.firebase_database.ModuleDescriptor.MODULE_VERSION;
+        }
+        Log.d(TAG, "unimplemented Method: getModuleVersion for " + moduleId);
+        return 0;
+    }
+}
