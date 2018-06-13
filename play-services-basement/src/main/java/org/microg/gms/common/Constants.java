64d76cee70bded4b2e45d43efbe76365bd7deb1b
Marvin W <git@larma.de>
2018-06-13 17:33:22 +0200

    Bump version code

--- play-services-basement/src/main/java/org/microg/gms/common/Constants.java
+++ play-services-basement/src/main/java/org/microg/gms/common/Constants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2017 microG Project Team
+ * Copyright (C) 2013-2018 microG Project Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 package org.microg.gms.common;
 
 public class Constants {
-    public static final int MAX_REFERENCE_VERSION = 12221000;
+    public static final int MAX_REFERENCE_VERSION = 12688 * 1000;
     public static final String GMS_PACKAGE_NAME = "com.google.android.gms";
     public static final String GSF_PACKAGE_NAME = "com.google.android.gsf";
     public static final String GMS_PACKAGE_SIGNATURE_SHA1 = "38918a453d07199354f8b19af05ec6562ced5788";
