feeff75ab10072c89cd17832bf53e859b946cfae
Adam Mills <adam@armills.info>
2018-08-25 18:49:17 -0400

    Constructor for CastDeviceStatus

--- play-services-cast-api/src/main/java/com/google/android/gms/cast/CastDeviceStatus.java
+++ play-services-cast-api/src/main/java/com/google/android/gms/cast/CastDeviceStatus.java
@@ -21,6 +21,17 @@ import org.microg.safeparcel.SafeParceled;
 
 public class CastDeviceStatus extends AutoSafeParcelable {
 
+    public CastDeviceStatus() {
+    }
+
+    public CastDeviceStatus(double volume, boolean mute, int activeInputState, ApplicationMetadata applicationMetadata, int standbyState) {
+        this.volume = volume;
+        this.mute = mute;
+        this.activeInputState = activeInputState;
+        this.applicationMetadata = applicationMetadata;
+        this.standbyState = standbyState;
+    }
+
     @SafeParceled(1)
     private int versionCode = 1;
     @SafeParceled(2)
