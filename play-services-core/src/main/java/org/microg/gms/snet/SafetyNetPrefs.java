93ce89444bb818b32be77714ca9b76c57734e8de
Christopher Roy Bratusek <nano@jpberlin.de>
2018-10-03 18:39:58 +0200

    After enabling SafetyNet set default value to official if it was never set otherwise

--- play-services-core/src/main/java/org/microg/gms/snet/SafetyNetPrefs.java
+++ play-services-core/src/main/java/org/microg/gms/snet/SafetyNetPrefs.java
@@ -57,7 +57,7 @@ public class SafetyNetPrefs implements SharedPreferences.OnSharedPreferenceChang
 
     public void update() {
         disabled = defaultPreferences.getBoolean(PREF_SNET_DISABLED, true);
-        official = defaultPreferences.getBoolean(PREF_SNET_OFFICIAL, true);
+        official = defaultPreferences.getBoolean(PREF_SNET_OFFICIAL, false);
         selfSigned = defaultPreferences.getBoolean(PREF_SNET_SELF_SIGNED, false);
         thirdParty = defaultPreferences.getBoolean(PREF_SNET_THIRD_PARTY, false);
         customUrl = defaultPreferences.getString(PREF_SNET_CUSTOM_URL, null);
