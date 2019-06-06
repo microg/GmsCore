2a2eacc5eb2ca8df286f59234b790baefed8aa96
chris42 <list-christian@web.de>
2019-06-06 10:57:16 +0200

    Remove ; Sort gmscore_out linking

--- Android.mk
+++ Android.mk
@@ -21,17 +21,20 @@ LOCAL_PACKAGE_NAME := GmsCore
 
 gmscore_root  := $(LOCAL_PATH)
 gmscore_dir   := play-services-core
-gmscore_out   := $(TARGET_COMMON_OUT_ROOT)/obj/APPS/$(LOCAL_MODULE)_intermediates
+gmscore_out   := out/target/common/obj/APPS/$(LOCAL_MODULE)_intermediates
 gmscore_build := $(gmscore_root)/$(gmscore_dir)/build
 gmscore_apk   := build/outputs/apk/release/play-services-core-release-unsigned.apk
 
-$(gmscore_root)/$(gmscore_dir)/$(gmscore_apk):
-	rm -Rf $(gmscore_build)
-	mkdir -p $(ANDROID_BUILD_TOP)/$(gmscore_out)
-	ln -s $(ANDROID_BUILD_TOP)/$(gmscore_out) $(ANDROID_BUILD_TOP)/$(gmscore_build)
-	echo "sdk.dir=$(ANDROID_HOME)" > $(gmscore_root)/local.properties
-	cd $(gmscore_root) && git submodule update --recursive --init
-	cd $(gmscore_root)/$(gmscore_dir) && JAVA_TOOL_OPTIONS="$(JAVA_TOOL_OPTIONS) -Dfile.encoding=UTF8" ../gradlew assembleRelease
+.PHONY: preps_gms
+preps_gms:
+	rm -rf $(gmscore_build); \
+	mkdir -p $(gmscore_out); \
+	ln -s ../../../../$(gmscore_out) $(gmscore_build)
+
+$(gmscore_root)/$(gmscore_dir)/$(gmscore_apk): preps_gms
+	echo "sdk.dir=$(ANDROID_HOME)" > $(gmscore_root)/local.properties; \
+	cd $(gmscore_root) && git submodule update --recursive --init; \
+	cd $(gmscore_dir) && JAVA_TOOL_OPTIONS="$(JAVA_TOOL_OPTIONS) -Dfile.encoding=UTF8" ../gradlew assembleRelease
 
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
