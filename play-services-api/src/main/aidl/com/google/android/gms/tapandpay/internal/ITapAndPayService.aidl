4c4277b75e843b759b0ff93dfb01c5e55cf41742
Marvin W <git@larma.de>
2019-10-08 20:55:50 +0200

    Add tap and pay dummy

---
+++ play-services-api/src/main/aidl/com/google/android/gms/tapandpay/internal/ITapAndPayService.aidl
@@ -0,0 +1,5 @@
+package com.google.android.gms.tapandpay.internal;
+
+interface ITapAndPayService {
+
+}
--- play-services-basement/src/main/java/org/microg/gms/common/GmsService.java
+++ play-services-basement/src/main/java/org/microg/gms/common/GmsService.java
@@ -88,6 +88,7 @@ public enum GmsService {
     PLUS_INTERNAL(70),
     SOURCE_DEVICE(75, "com.google.android.gms.smartdevice.d2d.SourceDeviceService.START"),
     TARGET_DEVICE(76, "com.google.android.gms.smartdevice.d2d.TargetDeviceService.START"),
+    TAP_AND_PAY(79, "com.google.android.gms.tapandpay.service.BIND"),
     ACCOUNTS(81, "com.google.android.gms.smartdevice.setup.accounts.AccountsService.START"),
     TRUST_AGENT(85, "com.google.android.gms.trustagent.StateApi.START"),
     MEASUREMENT(93, "com.google.android.gms.measurement.START"),
