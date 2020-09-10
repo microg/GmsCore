0a4fd6158a6338cc15a99c833f08fa345715e205
Oizaro <45825534+Oizaro@users.noreply.github.com>
2020-09-10 04:01:39 +0200

    Avoid notification coming back

--- play-services-core/src/main/java/org/microg/gms/common/StatusNotification.java
+++ play-services-core/src/main/java/org/microg/gms/common/StatusNotification.java
@@ -8,41 +8,39 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.PowerManager;
 import android.provider.Settings;
-import android.service.notification.StatusBarNotification;
 
 import androidx.core.app.NotificationCompat;
 
 import com.mgoogle.android.gms.R;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 public class StatusNotification {
 
     private static int notificationID;
+    private static boolean notificationExists = false;
 
     public static boolean Notify(Context context) {
         PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (!powerManager.isIgnoringBatteryOptimizations(context.getPackageName())) {
-                NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                StatusBarNotification[] notifications = manager.getActiveNotifications();
-                for (StatusBarNotification notification : notifications) {
-                    if (notification.getId() == notificationID) {
-                        return false;
-                    }
+                if (notificationExists) {
+                    return false;
                 }
 
                 buildStatusNotification(context);
+            } else {
+                if (notificationExists) {
+                    ((NotificationManager)
+                            context.getSystemService(Context.NOTIFICATION_SERVICE)).cancel(notificationID);
+                    notificationExists = false;
+                }
             }
         }
-        return true;
+        return false;
     }
 
     private static void buildStatusNotification(Context context) {
-        notificationID = createNotificationID();
+        notificationID = 1;
 
         Intent intent = new Intent();
         intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
@@ -52,29 +50,27 @@ public class StatusNotification {
         String notificationChannelID = "foreground-service";
 
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(notificationChannelID, context.getResources().getString(R.string.notification_service_name), NotificationManager.IMPORTANCE_LOW);
+            NotificationChannel channel = new NotificationChannel(notificationChannelID,
+                    context.getResources().getString(R.string.notification_service_name),
+                    NotificationManager.IMPORTANCE_LOW);
             channel.setShowBadge(true);
             channel.setLockscreenVisibility(0);
             channel.setVibrationPattern(new long[0]);
             context.getSystemService(NotificationManager.class).createNotificationChannel(channel);
         }
         NotificationCompat.Builder notification = new NotificationCompat.Builder(context, notificationChannelID)
-                .setAutoCancel(true)
+                .setOngoing(true)
                 .setContentIntent(pendingIntent)
-                .setContentTitle(context.getResources().getString(R.string.notification_service_title))
-                .setContentText(context.getResources().getString(R.string.notification_service_content))
-                .setSmallIcon(R.drawable.ic_foreground_notification);
+                .setSmallIcon(R.drawable.ic_foreground_notification)
+                .setSubText(context.getResources().getString(R.string.notification_service_title))
+                .setContentTitle(context.getResources().getString(R.string.notification_service_content))
+                .setContentText(context.getResources().getString(R.string.notification_service_subcontent));
 
         NotificationManager manager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         manager.notify(notificationID, notification.build());
-    }
 
-    private static int createNotificationID() {
-        Date currentDate = new Date();
-        int id = Integer.parseInt(new SimpleDateFormat("ddHHmmss",  Locale.US).format(currentDate));
-
-        return id;
+        notificationExists = true;
     }
 }
