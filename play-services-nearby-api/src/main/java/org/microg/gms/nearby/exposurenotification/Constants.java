412e513afe8e27c4d183c23e05f27ab752ead0b2
Marvin W <git@larma.de>
2021-02-21 16:39:12 -0600

    EN: Implement new versioning scheme of API 1.8

--- play-services-nearby-api/src/main/java/org/microg/gms/nearby/exposurenotification/Constants.java
+++ play-services-nearby-api/src/main/java/org/microg/gms/nearby/exposurenotification/Constants.java
@@ -5,6 +5,8 @@
 
 package org.microg.gms.nearby.exposurenotification;
 
+import static org.microg.gms.common.Constants.GMS_VERSION_CODE;
+
 public class Constants {
     public static final String ACTION_EXPOSURE_NOTIFICATION_SETTINGS = "com.google.android.gms.settings.EXPOSURE_NOTIFICATION_SETTINGS";
     public static final String ACTION_EXPOSURE_NOT_FOUND = "com.google.android.gms.exposurenotification.ACTION_EXPOSURE_NOT_FOUND";
@@ -17,4 +19,6 @@ public class Constants {
     public static final String EXTRA_TOKEN = "com.google.android.gms.exposurenotification.EXTRA_TOKEN";
     public static final String TOKEN_A = "TYZWQ32170AXEUVCDW7A";
     public static final int DAYS_SINCE_ONSET_OF_SYMPTOMS_UNKNOWN = Integer.MAX_VALUE;
+    public static final int VERSION = 18;
+    public static final long VERSION_FULL = ((long)VERSION) * 1000000000L + GMS_VERSION_CODE;
 }
