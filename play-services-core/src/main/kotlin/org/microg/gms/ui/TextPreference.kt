d231e059f7e62f98b967f4e0db50c318396d52c9
KevinX8 <pgnmaster24@gmail.com>
2022-01-31 12:49:59 +0000

    Update Dependencies and SDK target to match Vanced

--- play-services-core/src/main/kotlin/org/microg/gms/ui/TextPreference.kt
+++ play-services-core/src/main/kotlin/org/microg/gms/ui/TextPreference.kt
@@ -15,20 +15,21 @@ import androidx.preference.Preference
 import androidx.preference.PreferenceViewHolder
 
 class TextPreference : Preference {
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(
+        context!!, attrs, defStyleAttr, defStyleRes)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context!!, attrs, defStyleAttr)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs)
     constructor(context: Context) : super(context)
 
 
-    override fun onBindViewHolder(holder: PreferenceViewHolder?) {
+    override fun onBindViewHolder(holder: PreferenceViewHolder) {
         super.onBindViewHolder(holder)
-        val iconFrame = holder?.findViewById(androidx.preference.R.id.icon_frame)
+        val iconFrame = holder.findViewById(androidx.preference.R.id.icon_frame)
         iconFrame?.layoutParams?.height = MATCH_PARENT
         (iconFrame as? LinearLayout)?.gravity = Gravity.TOP or Gravity.START
         val pad = (context.resources.displayMetrics.densityDpi/160f * 20).toInt()
         iconFrame?.setPadding(0, pad, 0, pad)
-        val textView = holder?.findViewById(android.R.id.summary) as? TextView
+        val textView = holder.findViewById(android.R.id.summary) as? TextView
         textView?.maxLines = Int.MAX_VALUE
     }
 }
