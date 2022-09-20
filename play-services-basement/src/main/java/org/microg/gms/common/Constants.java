44f1a9e0b124e8535312b0165f164a7d9454ffd2
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2022-09-20 20:01:11 +0200

    Add secondary package signature

--- play-services-basement/src/main/java/org/microg/gms/common/Constants.java
+++ play-services-basement/src/main/java/org/microg/gms/common/Constants.java
@@ -23,6 +23,7 @@ public class Constants {
     public static final String GMS_PACKAGE_NAME = "com.google.android.gms";
     public static final String GSF_PACKAGE_NAME = "com.google.android.gsf";
     public static final String GMS_PACKAGE_SIGNATURE_SHA1 = "38918a453d07199354f8b19af05ec6562ced5788";
+    public static final String GMS_SECONDARY_PACKAGE_SIGNATURE_SHA1 = "bd32424203e0fb25f36b57e5aa356f9bdd1da998";
     @Deprecated
     public static final int MAX_REFERENCE_VERSION = GMS_VERSION_CODE;
 }
