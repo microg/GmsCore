2a19ef45de5ad43bb3101f72aff477a2fc491e68
Marvin W <git@larma.de>
2022-10-30 17:53:10 -0600

    Fix crash during sign-in when SafetyNet is disabled

--- play-services-core/src/main/kotlin/org/microg/gms/auth/login/DroidGuardHandler.kt
+++ play-services-core/src/main/kotlin/org/microg/gms/auth/login/DroidGuardHandler.kt
@@ -14,9 +14,13 @@ import java.util.*
 class DroidGuardHandler(private val activity: LoginActivity) {
     fun start(dg: String) {
         activity.lifecycleScope.launchWhenStarted {
-            val result = getResult(activity, "minute_maid", Collections.singletonMap("dg_minutemaid", dg))
-                .toBase64(Base64.NO_WRAP, Base64.NO_PADDING, Base64.URL_SAFE)
-            activity.runScript("window.setDgResult('$result')");
+            try {
+                val result = getResult(activity, "minute_maid", Collections.singletonMap("dg_minutemaid", dg))
+                    .toBase64(Base64.NO_WRAP, Base64.NO_PADDING, Base64.URL_SAFE)
+                activity.runScript("window.setDgResult('$result')")
+            } catch (e: Exception) {
+                // Ignore
+            }
         }
     }
 }
