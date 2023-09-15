0f04df65e0deca5ca58b6d377a464d27f669f06e
Marvin W <git@larma.de>
2023-09-15 09:36:38 +0200

    Use fake-signature module
    This is the same as in fake-store. Soon we'll have to move FakeStore/Companion
    to this repository anyway (so that it can access certain parts from core needed
    to support IAP and PI), until then it's probably fine to have this code
    duplicated.

---
+++ fake-signature/src/main/java/com/huawei/signature/diff/InitProvider.java
@@ -0,0 +1,69 @@
+/*
+ * SPDX-FileCopyrightText: 2023 microG Project Team
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.huawei.signature.diff;
+
+import android.app.ActivityManager;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+import java.util.List;
+
+public class InitProvider extends ContentProvider {
+    private static final String TAG = InitProvider.class.getSimpleName();
+
+    @Override
+    public boolean onCreate() {
+        Log.d(TAG, "onCreate");
+        if (!isServiceRunning(getContext(), getContext().getPackageName(), SignatureService.class.getName())) {
+            Intent intent = new Intent(getContext(), SignatureService.class);
+            getContext().startService(intent);
+        }
+        return false;
+    }
+
+    private boolean isServiceRunning(Context context, String packageName, String serviceName) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> serviceInfoList = manager.getRunningServices(Integer.MAX_VALUE);
+        if (serviceInfoList == null) {
+            return false;
+        }
+        for (ActivityManager.RunningServiceInfo info : serviceInfoList) {
+            if (info.service.getPackageName().equals(packageName) && info.service.getClassName().equals(serviceName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
