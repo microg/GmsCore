c10421187b1f1f6b24c4c272d739f23227528799
Marvin W <git@larma.de>
2023-12-24 02:32:36 +0100

    Add support for MLKit Vision Barcode API
    Also improve Play Services Vision Barcode API

--- play-services-vision/core/src/main/kotlin/org/microg/gms/vision/barcode/BarcodeScanner.kt
+++ play-services-vision/core/src/main/kotlin/org/microg/gms/vision/barcode/BarcodeScanner.kt
@@ -1,53 +1,59 @@
 /*
- * SPDX-FileCopyrightText: 2020, microG Project Team
+ * SPDX-FileCopyrightText: 2023 microG Project Team
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.microg.gms.vision.barcode
 
 import android.content.Context
-import android.graphics.Bitmap
+import android.graphics.ImageFormat
 import android.graphics.Point
+import android.media.Image
+import android.os.Build.VERSION.SDK_INT
+import android.os.Parcel
 import android.util.Log
 import com.google.android.gms.dynamic.IObjectWrapper
 import com.google.android.gms.dynamic.ObjectWrapper
 import com.google.android.gms.dynamic.unwrap
-import com.google.android.gms.vision.barcode.Barcode
-import com.google.android.gms.vision.barcode.internal.client.BarcodeDetectorOptions
-import com.google.android.gms.vision.barcode.internal.client.INativeBarcodeDetector
-import com.google.android.gms.vision.internal.FrameMetadataParcel
-import com.google.zxing.*
+import com.google.mlkit.vision.barcode.aidls.IBarcodeScanner
+import com.google.mlkit.vision.barcode.internal.*
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.Result
 import com.google.zxing.client.result.*
+import org.microg.gms.utils.warnOnTransactionIssues
 import java.nio.ByteBuffer
-import java.nio.IntBuffer
 import java.util.*
 
-private const val TAG = "BarcodeDetector"
+private const val TAG = "BarcodeScanner"
 
-class BarcodeDetector(val context: Context, val options: BarcodeDetectorOptions) : INativeBarcodeDetector.Stub() {
-    private val helper = BarcodeDecodeHelper(options.formats.gmsToZXingBarcodeFormats())
+class BarcodeScanner(val context: Context, val options: BarcodeScannerOptions) : IBarcodeScanner.Stub() {
+    private val helper =
+        BarcodeDecodeHelper(if (options.allPotentialBarcodesEnabled) BarcodeFormat.values().toList() else options.supportedFormats.mlKitToZXingBarcodeFormats())
     private var loggedOnce = false
 
-    override fun detectBitmap(wrappedBitmap: IObjectWrapper, metadata: FrameMetadataParcel): Array<Barcode> {
-        if (!loggedOnce) Log.d(TAG, "detectBitmap(${ObjectWrapper.unwrap(wrappedBitmap)}, $metadata)").also { loggedOnce = true }
-        val bitmap = wrappedBitmap.unwrap<Bitmap>() ?: return emptyArray()
-        return helper.decodeFromBitmap(bitmap)
-            .mapNotNull { runCatching { it.toGms(metadata) }.getOrNull() }.toTypedArray()
-    }
-
-    override fun detectBytes(wrappedByteBuffer: IObjectWrapper, metadata: FrameMetadataParcel): Array<Barcode> {
-        if (!loggedOnce) Log.d(TAG, "detectBytes(${ObjectWrapper.unwrap(wrappedByteBuffer)}, $metadata)").also { loggedOnce = true }
-        val bytes = wrappedByteBuffer.unwrap<ByteBuffer>() ?: return emptyArray()
-        return helper.decodeFromLuminanceBytes(bytes, metadata.width, metadata.height)
-            .mapNotNull { runCatching { it.toGms(metadata) }.getOrNull() }.toTypedArray()
+    override fun init() {
+        Log.d(TAG, "init()")
     }
 
     override fun close() {
         Log.d(TAG, "close()")
     }
+
+    override fun detect(wrappedImage: IObjectWrapper, metadata: ImageMetadata): List<Barcode> {
+        if (!loggedOnce) Log.d(TAG, "detect(${ObjectWrapper.unwrap(wrappedImage)}, $metadata)").also { loggedOnce = true }
+        return when (metadata.format) {
+            ImageFormat.NV21 -> wrappedImage.unwrap<ByteBuffer>()?.let { helper.decodeFromLuminanceBytes(it, metadata.width, metadata.height) }
+            ImageFormat.YUV_420_888 -> if (SDK_INT >= 19) wrappedImage.unwrap<Image>()?.let { image -> helper.decodeFromImage(image) } else null
+
+            else -> null
+        }?.map { it.toMlKit(metadata) } ?: emptyList()
+    }
+
+    override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean =
+        warnOnTransactionIssues(code, reply, flags, TAG) { super.onTransact(code, data, reply, flags) }
 }
 
-private fun Int.gmsToZXingBarcodeFormats(): List<BarcodeFormat> {
+private fun Int.mlKitToZXingBarcodeFormats(): List<BarcodeFormat> {
     return listOfNotNull(
         BarcodeFormat.AZTEC.takeIf { (this and Barcode.AZTEC) > 0 || this == Barcode.ALL_FORMATS },
         BarcodeFormat.CODABAR.takeIf { (this and Barcode.CODABAR) > 0 || this == Barcode.ALL_FORMATS },
@@ -65,25 +71,24 @@ private fun Int.gmsToZXingBarcodeFormats(): List<BarcodeFormat> {
     )
 }
 
-
-private fun BarcodeFormat.toGms(): Int = when (this) {
+private fun BarcodeFormat.toMlKit(): Int = when (this) {
     BarcodeFormat.AZTEC -> Barcode.AZTEC
     BarcodeFormat.CODABAR -> Barcode.CODABAR
     BarcodeFormat.CODE_39 -> Barcode.CODE_39
     BarcodeFormat.CODE_93 -> Barcode.CODE_93
     BarcodeFormat.CODE_128 -> Barcode.CODE_128
     BarcodeFormat.DATA_MATRIX -> Barcode.DATA_MATRIX
-    BarcodeFormat.EAN_13 -> Barcode.EAN_13
     BarcodeFormat.EAN_8 -> Barcode.EAN_8
+    BarcodeFormat.EAN_13 -> Barcode.EAN_13
     BarcodeFormat.ITF -> Barcode.ITF
     BarcodeFormat.PDF_417 -> Barcode.PDF417
     BarcodeFormat.QR_CODE -> Barcode.QR_CODE
     BarcodeFormat.UPC_A -> Barcode.UPC_A
     BarcodeFormat.UPC_E -> Barcode.UPC_E
-    else -> throw UnsupportedOperationException()
+    else -> Barcode.UNKNOWN_FORMAT
 }
 
-private fun ParsedResultType.toGms(): Int = when (this) {
+private fun ParsedResultType.toMlKit(): Int = when (this) {
     ParsedResultType.ADDRESSBOOK -> Barcode.CONTACT_INFO
     ParsedResultType.CALENDAR -> Barcode.CALENDAR_EVENT
     ParsedResultType.EMAIL_ADDRESS -> Barcode.EMAIL
@@ -95,43 +100,43 @@ private fun ParsedResultType.toGms(): Int = when (this) {
     ParsedResultType.TEXT -> Barcode.TEXT
     ParsedResultType.URI -> Barcode.URL
     ParsedResultType.WIFI -> Barcode.WIFI
-    else -> Barcode.TEXT
+    else -> Barcode.UNKNOWN_TYPE
 }
 
-private fun AddressBookParsedResult.toGms(): Barcode.ContactInfo {
-    val contactInfo = Barcode.ContactInfo()
+private fun AddressBookParsedResult.toMlKit(): ContactInfo {
+    val contactInfo = ContactInfo()
     // TODO: contactInfo.name
     contactInfo.organization = org
     contactInfo.title = title
     contactInfo.phones = phoneNumbers.orEmpty().mapIndexed { i, a ->
-        Barcode.Phone().apply {
+        Phone().apply {
             type = when (phoneTypes?.getOrNull(i)) {
-                "WORK" -> Barcode.Phone.WORK
-                "HOME" -> Barcode.Phone.HOME
-                "FAX" -> Barcode.Phone.FAX
-                "MOBILE" -> Barcode.Phone.MOBILE
-                else -> Barcode.Phone.UNKNOWN
+                "WORK" -> Phone.WORK
+                "HOME" -> Phone.HOME
+                "FAX" -> Phone.FAX
+                "MOBILE" -> Phone.MOBILE
+                else -> Phone.UNKNOWN
             }
             number = a
         }
     }.toTypedArray()
     contactInfo.emails = emails.orEmpty().mapIndexed { i, a ->
-        Barcode.Email().apply {
+        Email().apply {
             type = when (emailTypes?.getOrNull(i)) {
-                "WORK" -> Barcode.Email.WORK
-                "HOME" -> Barcode.Email.HOME
-                else -> Barcode.Email.UNKNOWN
+                "WORK" -> Email.WORK
+                "HOME" -> Email.HOME
+                else -> Email.UNKNOWN
             }
             address = a
         }
     }.toTypedArray()
     contactInfo.urls = urLs
     contactInfo.addresses = addresses.orEmpty().mapIndexed { i, a ->
-        Barcode.Address().apply {
+        Address().apply {
             type = when (addressTypes?.getOrNull(i)) {
-                "WORK" -> Barcode.Address.WORK
-                "HOME" -> Barcode.Address.HOME
-                else -> Barcode.Address.UNKNOWN
+                "WORK" -> Address.WORK
+                "HOME" -> Address.HOME
+                else -> Address.UNKNOWN
             }
             addressLines = a.split("\n").toTypedArray()
         }
@@ -140,8 +145,8 @@ private fun AddressBookParsedResult.toGms(): Barcode.ContactInfo {
     return contactInfo
 }
 
-private fun CalendarParsedResult.toGms(): Barcode.CalendarEvent {
-    fun createDateTime(timestamp: Long, isAllDay: Boolean) = Barcode.CalendarDateTime().apply {
+private fun CalendarParsedResult.toMlKit(): CalendarEvent {
+    fun createDateTime(timestamp: Long, isAllDay: Boolean) = CalendarDateTime().apply {
         val calendar = Calendar.getInstance()
         calendar.time = Date(timestamp)
         year = calendar.get(Calendar.YEAR)
@@ -159,7 +164,7 @@ private fun CalendarParsedResult.toGms(): Barcode.CalendarEvent {
     }
 
 
-    val event = Barcode.CalendarEvent()
+    val event = CalendarEvent()
     event.summary = summary
     event.description = description
     event.location = location
@@ -169,58 +174,59 @@ private fun CalendarParsedResult.toGms(): Barcode.CalendarEvent {
     return event
 }
 
-private fun EmailAddressParsedResult.toGms(): Barcode.Email {
-    val email = Barcode.Email()
+private fun EmailAddressParsedResult.toMlKit(): Email {
+    val email = Email()
     email.address = tos?.getOrNull(0)
     email.subject = subject
     email.body = body
     return email
 }
 
-private fun GeoParsedResult.toGms(): Barcode.GeoPoint {
-    val geo = Barcode.GeoPoint()
+private fun GeoParsedResult.toMlKit(): GeoPoint {
+    val geo = GeoPoint()
     geo.lat = latitude
     geo.lng = longitude
     return geo
 }
 
-private fun TelParsedResult.toGms(): Barcode.Phone {
-    val phone = Barcode.Phone()
+private fun TelParsedResult.toMlKit(): Phone {
+    val phone = Phone()
     phone.number = number
     return phone
 }
 
-private fun SMSParsedResult.toGms(): Barcode.Sms {
-    val sms = Barcode.Sms()
+private fun SMSParsedResult.toMlKit(): Sms {
+    val sms = Sms()
     sms.message = body
     sms.phoneNumber = numbers?.getOrNull(0)
     return sms
 }
 
-private fun WifiParsedResult.toGms(): Barcode.WiFi {
-    val wifi = Barcode.WiFi()
+private fun WifiParsedResult.toMlKit(): WiFi {
+    val wifi = WiFi()
     wifi.ssid = ssid
     wifi.password = password
     wifi.encryptionType = when (networkEncryption) {
-        "OPEN" -> Barcode.WiFi.OPEN
-        "WEP" -> Barcode.WiFi.WEP
-        "WPA" -> Barcode.WiFi.WPA
-        "WPA2" -> Barcode.WiFi.WPA
+        "OPEN" -> WiFi.OPEN
+        "WEP" -> WiFi.WEP
+        "WPA" -> WiFi.WPA
+        "WPA2" -> WiFi.WPA
         else -> 0
     }
     return wifi
 }
 
-private fun URIParsedResult.toGms(): Barcode.UrlBookmark {
-    val url = Barcode.UrlBookmark()
+
+private fun URIParsedResult.toMlKit(): UrlBookmark {
+    val url = UrlBookmark()
     url.url = uri
     url.title = title
     return url
 }
 
-private fun Result.toGms(metadata: FrameMetadataParcel): Barcode {
+private fun Result.toMlKit(metadata: ImageMetadata): Barcode {
     val barcode = Barcode()
-    barcode.format = barcodeFormat.toGms()
+    barcode.format = barcodeFormat.toMlKit()
     barcode.rawBytes = rawBytes
     barcode.rawValue = text
     barcode.cornerPoints = resultPoints.map {
@@ -235,31 +241,31 @@ private fun Result.toGms(metadata: FrameMetadataParcel): Barcode {
     val parsed = ResultParser.parseResult(this)
 
     barcode.displayValue = parsed.displayResult
-    barcode.valueFormat = parsed.type.toGms()
+    barcode.valueType = parsed.type.toMlKit()
     when (parsed) {
         is EmailAddressParsedResult ->
-            barcode.email = parsed.toGms()
+            barcode.email = parsed.toMlKit()
 
         is TelParsedResult ->
-            barcode.phone = parsed.toGms()
+            barcode.phone = parsed.toMlKit()
 
         is SMSParsedResult ->
-            barcode.sms = parsed.toGms()
+            barcode.sms = parsed.toMlKit()
 
         is WifiParsedResult ->
-            barcode.wifi = parsed.toGms()
+            barcode.wifi = parsed.toMlKit()
 
         is URIParsedResult ->
-            barcode.url = parsed.toGms()
+            barcode.urlBookmark = parsed.toMlKit()
 
         is GeoParsedResult ->
-            barcode.geoPoint = parsed.toGms()
+            barcode.geoPoint = parsed.toMlKit()
 
         is CalendarParsedResult ->
-            barcode.calendarEvent = parsed.toGms()
+            barcode.calendarEvent = parsed.toMlKit()
 
         is AddressBookParsedResult ->
-            barcode.contactInfo = parsed.toGms()
+            barcode.contactInfo = parsed.toMlKit()
     }
 
     return barcode
