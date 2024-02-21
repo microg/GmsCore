33c7f20f229dbc906f783f2f71792ebaf1bae285
S1m <git@sgougeon.fr>
2024-02-21 13:09:11 +0100

    Fido: Add all features

--- play-services-fido/core/src/main/kotlin/org/microg/gms/fido/core/privileged/Fido2PrivilegedService.kt
+++ play-services-fido/core/src/main/kotlin/org/microg/gms/fido/core/privileged/Fido2PrivilegedService.kt
@@ -17,7 +17,6 @@ import androidx.core.app.PendingIntentCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
-import com.google.android.gms.common.Feature
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.common.api.Status
 import com.google.android.gms.common.internal.ConnectionInfo
@@ -32,6 +31,7 @@ import com.google.android.gms.fido.fido2.internal.privileged.IFido2PrivilegedSer
 import org.microg.gms.BaseService
 import org.microg.gms.common.GmsService
 import org.microg.gms.common.GmsService.FIDO2_PRIVILEGED
+import org.microg.gms.fido.core.FEATURES
 import org.microg.gms.fido.core.ui.AuthenticatorActivity
 import org.microg.gms.fido.core.ui.AuthenticatorActivity.Companion.SOURCE_BROWSER
 import org.microg.gms.fido.core.ui.AuthenticatorActivity.Companion.KEY_SOURCE
@@ -49,12 +49,7 @@ class Fido2PrivilegedService : BaseService(TAG, FIDO2_PRIVILEGED) {
         callback.onPostInitCompleteWithConnectionInfo(
             CommonStatusCodes.SUCCESS,
             Fido2PrivilegedServiceImpl(this, lifecycle).asBinder(),
-            ConnectionInfo().apply {
-                features = arrayOf(
-                    Feature("is_user_verifying_platform_authenticator_available", 1),
-                    Feature("is_user_verifying_platform_authenticator_available_for_credential", 1)
-                )
-            }
+            ConnectionInfo().apply { features = FEATURES }
         );
     }
 }
