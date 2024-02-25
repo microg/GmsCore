abe818a50c255b044de1d5234dc568c58597d928
S1m <git@sgougeon.fr>
2024-02-25 11:58:11 +0100

    Add dialog to inform user about minus services

--- play-services-core/src/main/java/org/microg/gms/ui/MainSettingsActivity.java
+++ play-services-core/src/main/java/org/microg/gms/ui/MainSettingsActivity.java
@@ -1,9 +1,11 @@
 package org.microg.gms.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -11,8 +13,11 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.gms.BuildConfig;
 import com.google.android.gms.R;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import org.microg.gms.common.Constants;
 import org.microg.gms.ui.settings.SettingsProvider;
 
 import static org.microg.gms.ui.settings.SettingsProviderKt.getAllSettingsProviders;
@@ -20,8 +25,27 @@ import static org.microg.gms.ui.settings.SettingsProviderKt.getAllSettingsProvid
 public class MainSettingsActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
 
+    private static final String FirstRunMaster = "FirstRun";
+    private static final String FirstRunPref = "FirstRun_pref";
+
     private NavController getNavController() {
         return ((NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.navhost)).getNavController();
+    }
+
+    private void showDialogIfNeeded() {
+        SharedPreferences prefs = getSharedPreferences(FirstRunMaster, MODE_PRIVATE);
+        if (BuildConfig.APPLICATION_ID == Constants.GMS_MINUS_PACKAGE_NAME &&
+                prefs.getBoolean(FirstRunPref, true)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setMessage(R.string.minus_dialog_information)
+                    .setTitle(R.string.gms_minus_app_name)
+                    .setPositiveButton(R.string.minus_dialog_information_ack, (dialog, id) -> {
+                        prefs.edit().putBoolean(FirstRunPref, false).apply();
+                    });
+
+            builder.create().show();
+        }
     }
 
     @Override
@@ -45,6 +69,7 @@ public class MainSettingsActivity extends AppCompatActivity {
 
         appBarConfiguration = new AppBarConfiguration.Builder(getNavController().getGraph()).build();
         NavigationUI.setupWithNavController(toolbarLayout, toolbar, getNavController(), appBarConfiguration);
+        showDialogIfNeeded();
     }
 
     @Override
