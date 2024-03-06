47bec1c64676d6c70525be618dc48898479b285e
davinci9196 <davinci9196@gmail.com>
2024-03-06 09:50:58 +0800

    Fix Google Drive jump to backup and repair page crash exception

--- play-services-core/src/main/kotlin/org/microg/gms/backup/component/BackupSettingsCollapsingActivity.kt
+++ play-services-core/src/main/kotlin/org/microg/gms/backup/component/BackupSettingsCollapsingActivity.kt
@@ -5,29 +5,14 @@
 package org.microg.gms.backup.component
 
 import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import com.google.android.gms.R
-import org.microg.gms.profile.Build
-import org.microg.gms.profile.ProfileManager
-
-private val ACTION_BACKUP = hashMapOf(
-    "HUAWEI" to "com.huawei.KoBackup.StartHwBackup"
-)
 
 class BackupSettingsCollapsingActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ProfileManager.ensureInitialized(this)
-
-        val intent = Intent()
-        intent.setAction(ACTION_BACKUP[Build.BRAND])
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        } else {
-            Toast.makeText(this, getString(R.string.backup_disabled), Toast.LENGTH_SHORT).show()
-        }
+        Toast.makeText(this, getString(R.string.backup_disabled), Toast.LENGTH_SHORT).show()
         finish()
     }
 }
