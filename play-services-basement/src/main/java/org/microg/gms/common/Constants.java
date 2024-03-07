4b3a32c69e1a63202634a59f8d99ad77cecf58a0
S1m <git@sgougeon.fr>
2024-03-07 08:45:26 +0100

    Rename limited services

--- play-services-basement/src/main/java/org/microg/gms/common/Constants.java
+++ play-services-basement/src/main/java/org/microg/gms/common/Constants.java
@@ -21,7 +21,7 @@ import com.google.android.gms.common.BuildConfig;
 public class Constants {
     public static final int GMS_VERSION_CODE = (BuildConfig.VERSION_CODE / 1000) * 1000;
     public static final String GMS_PACKAGE_NAME = "com.google.android.gms";
-    public static final String GMS_MINUS_PACKAGE_NAME = "org.microg.gms";
+    public static final String MICROG_PACKAGE_NAME = "org.microg.gms";
     public static final String GSF_PACKAGE_NAME = "com.google.android.gsf";
     public static final String GMS_PACKAGE_SIGNATURE_SHA1 = "38918a453d07199354f8b19af05ec6562ced5788";
     public static final String GMS_SECONDARY_PACKAGE_SIGNATURE_SHA1 = "bd32424203e0fb25f36b57e5aa356f9bdd1da998";
