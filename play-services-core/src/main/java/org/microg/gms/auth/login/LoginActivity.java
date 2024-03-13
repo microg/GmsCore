e46af40b7bd3498cffe76651a910656ab39256e1
davinci9196 <davinci9196@gmail.com>
2024-03-13 20:40:28 +0800

    Fix the problem that auth_manager_visible does not take effect

--- play-services-core/src/main/java/org/microg/gms/auth/login/LoginActivity.java
+++ play-services-core/src/main/java/org/microg/gms/auth/login/LoginActivity.java
@@ -304,9 +304,6 @@ public class LoginActivity extends AssistantActivity {
                             accountManager.setUserData(account, "lastName", response.lastName);
                             if (!TextUtils.isEmpty(response.accountId))
                                 accountManager.setUserData(account, "GoogleUserId", response.accountId);
-                            if (isAuthVisible(LoginActivity.this) && SDK_INT >= 26) {
-                                accountManager.setAccountVisibility(account, PACKAGE_NAME_KEY_LEGACY_NOT_VISIBLE, VISIBILITY_USER_MANAGED_VISIBLE);
-                            }
                             retrieveGmsToken(account);
                             setResult(RESULT_OK);
                         } else {
@@ -351,6 +348,9 @@ public class LoginActivity extends AssistantActivity {
                         String accountId = PeopleManager.loadUserInfo(LoginActivity.this, account);
                         if (!TextUtils.isEmpty(accountId))
                             accountManager.setUserData(account, "GoogleUserId", accountId);
+                        if (isAuthVisible(LoginActivity.this) && SDK_INT >= 26) {
+                            accountManager.setAccountVisibility(account, PACKAGE_NAME_KEY_LEGACY_NOT_VISIBLE, VISIBILITY_USER_MANAGED_VISIBLE);
+                        }
                         checkin(true);
                         finish();
                     }
