1df40b817edb7ee4457bcba41a915960cc9431f7
Marvin W <git@larma.de>
2024-04-29 18:18:05 +0200

    Location: Add support for requesting background location permission on Huawei

--- play-services-location/core/src/main/kotlin/org/microg/gms/location/manager/AskPermissionActivity.kt
+++ play-services-location/core/src/main/kotlin/org/microg/gms/location/manager/AskPermissionActivity.kt
@@ -12,10 +12,12 @@ import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.os.Message
 import android.os.Messenger
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
+import org.microg.gms.location.core.BuildConfig
 
 const val EXTRA_MESSENGER = "messenger"
 const val EXTRA_PERMISSIONS = "permissions"
@@ -32,7 +34,13 @@ class AskPermissionActivity : AppCompatActivity() {
     }
 
     private fun requestPermissions(permissions: Array<String> = intent?.getStringArrayExtra(EXTRA_PERMISSIONS) ?: emptyArray()) {
-        permissionGrants = permissions.map { ContextCompat.checkSelfPermission(this, it) }.toIntArray()
+        permissionGrants = permissions.map {
+            if (BuildConfig.FORCE_SHOW_BACKGROUND_PERMISSION.isNotEmpty() && BuildConfig.FORCE_SHOW_BACKGROUND_PERMISSION == it) {
+                PackageManager.PERMISSION_GRANTED
+            } else {
+                ContextCompat.checkSelfPermission(this, it)
+            }
+        }.toIntArray()
         ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_PERMISSION)
     }
 
@@ -52,9 +60,16 @@ class AskPermissionActivity : AppCompatActivity() {
         if (requestCode == REQUEST_CODE_PERMISSION) {
             if (SDK_INT >= 30) {
                 val backgroundRequested = permissions.contains(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                if (BuildConfig.FORCE_SHOW_BACKGROUND_PERMISSION.isNotEmpty() && permissions.contains(BuildConfig.FORCE_SHOW_BACKGROUND_PERMISSION)) {
+                    grantResults[permissions.indexOf(BuildConfig.FORCE_SHOW_BACKGROUND_PERMISSION)] =
+                        PackageManager.PERMISSION_GRANTED
+                }
+                grantResults.forEach { Log.d(TAG, "onRequestPermissionsResult: $it") }
+                permissionGrants.forEach { Log.d(TAG, "onRequestPermissionsResult permissionGrants: $it") }
                 val backgroundDenied = backgroundRequested && grantResults[permissions.indexOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION)] == PackageManager.PERMISSION_DENIED
                 val onlyBackgroundDenied = backgroundDenied && grantResults.count { it == PackageManager.PERMISSION_DENIED } == 1
                 val someAccepted = !permissionGrants.contentEquals(grantResults)
+                Log.d(TAG, "onRequestPermissionsResult onlyBackgroundDenied: $onlyBackgroundDenied someAccepted:$someAccepted")
                 if (onlyBackgroundDenied && someAccepted) {
                     // Only background denied, ask again as some systems require that
                     requestPermissions()
