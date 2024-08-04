4a81ee629bdb6a43ad0d71ac7581961f95a3be14
DaVinci9196 <150454414+DaVinci9196@users.noreply.github.com>
2024-08-04 15:01:11 +0800

    Link PurchaseActivity to WebView of corresponding purchase page (#2443)
    Fix Google TV, Google Books in-app purchases

--- vending-app/src/main/java/org/microg/vending/billing/PurchaseActivity.java
+++ vending-app/src/main/java/org/microg/vending/billing/PurchaseActivity.java
@@ -5,20 +5,58 @@
 
 package org.microg.vending.billing;
 
+import static org.microg.vending.billing.ui.PlayWebViewActivityKt.KEY_WEB_VIEW_ACCOUNT;
+import static org.microg.vending.billing.ui.PlayWebViewActivityKt.KEY_WEB_VIEW_ACTION;
+import static org.microg.vending.billing.ui.PlayWebViewActivityKt.KEY_WEB_VIEW_OPEN_URL;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import com.android.vending.R;
 
-public class PurchaseActivity extends Activity {
+import org.microg.gms.auth.AuthConstants;
+import org.microg.vending.billing.ui.PlayWebViewActivity;
+import org.microg.vending.billing.ui.WebViewAction;
 
+public class PurchaseActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, getString(R.string.pay_disabled), Toast.LENGTH_SHORT).show();
+        String authAccount = getIntent().getStringExtra("authAccount");
+        AccountManager accountManager = AccountManager.get(this);
+        Account[] accounts = accountManager.getAccountsByType(AuthConstants.DEFAULT_ACCOUNT_TYPE);
+        String referralUrl = getIntent().getStringExtra("referral_url");
+        if (!TextUtils.isEmpty(referralUrl) && accounts.length > 0) {
+            Account currAccount = null;
+            for (Account account : accounts) {
+                if (account.name.equals(authAccount)) {
+                    currAccount = account;
+                    break;
+                }
+            }
+            if (!referralUrl.startsWith("https")) {
+                referralUrl = referralUrl.replace("http", "https");
+            }
+            //Perform host judgment on URLs to prevent risky URLs from being exploited
+            if (referralUrl.startsWith("https://play.google.com/store")) {
+                Intent intent = new Intent(this, PlayWebViewActivity.class);
+                intent.putExtra(KEY_WEB_VIEW_ACTION, WebViewAction.OPEN_GP_PRODUCT_DETAIL.toString());
+                intent.putExtra(KEY_WEB_VIEW_OPEN_URL, referralUrl);
+                intent.putExtra(KEY_WEB_VIEW_ACCOUNT, currAccount);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, getString(R.string.pay_disabled), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, getString(R.string.pay_disabled), Toast.LENGTH_SHORT).show();
+        }
         finish();
     }
 }
