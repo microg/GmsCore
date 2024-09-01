10ded90a8aa849bc57f6a01706993b0f21c266c0
Fynn Godau <fynngodau@mailbox.org>
2024-09-01 18:34:23 +0200

    Call account creation when app is using API to create work account

--- play-services-auth-workaccount/core/src/main/kotlin/org/microg/gms/auth/workaccount/WorkAccountService.kt
+++ play-services-auth-workaccount/core/src/main/kotlin/org/microg/gms/auth/workaccount/WorkAccountService.kt
@@ -5,10 +5,13 @@ import android.accounts.AccountManager
 import android.content.ComponentName
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Bundle
 import android.os.Parcel
 import android.util.Log
 import com.google.android.gms.auth.account.IWorkAccountCallback
 import com.google.android.gms.auth.account.IWorkAccountService
+import com.google.android.gms.auth.account.authenticator.WorkAccountAuthenticator.Companion.KEY_ACCOUNT_CREATION_TOKEN
+import com.google.android.gms.auth.account.authenticator.WorkAccountAuthenticator.Companion.WORK_ACCOUNT_TYPE
 import com.google.android.gms.common.Feature
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.common.internal.ConnectionInfo
@@ -20,14 +23,25 @@ import org.microg.gms.common.GmsService
 private const val TAG = "GmsWorkAccountService"
 
 class WorkAccountService : BaseService(TAG, GmsService.WORK_ACCOUNT) {
-    override fun handleServiceRequest(callback: IGmsCallbacks, request: GetServiceRequest, service: GmsService) {
-        callback.onPostInitCompleteWithConnectionInfo(CommonStatusCodes.SUCCESS, WorkAccountServiceImpl(this), ConnectionInfo().apply {
-            features = arrayOf(Feature("work_account_client_is_whitelisted", 1))
-        } )
+    override fun handleServiceRequest(
+        callback: IGmsCallbacks,
+        request: GetServiceRequest,
+        service: GmsService
+    ) {
+        callback.onPostInitCompleteWithConnectionInfo(
+            CommonStatusCodes.SUCCESS,
+            WorkAccountServiceImpl(this),
+            ConnectionInfo().apply {
+                features = arrayOf(Feature("work_account_client_is_whitelisted", 1))
+            })
     }
 }
 
-class WorkAccountServiceImpl(val context: Context) : IWorkAccountService.Stub() {
+class WorkAccountServiceImpl(context: Context) : IWorkAccountService.Stub() {
+
+    val packageManager: PackageManager = context.packageManager
+    val accountManager: AccountManager = AccountManager.get(context)
+
     override fun onTransact(code: Int, data: Parcel, reply: Parcel?, flags: Int): Boolean {
         Log.d(TAG, "$code, $data, $reply, $flags")
         return super.onTransact(code, data, reply, flags)
@@ -40,20 +54,42 @@ class WorkAccountServiceImpl(val context: Context) : IWorkAccountService.Stub() 
         val sharedPreferences = context.getSharedPreferences("work_account_prefs", Context.MODE_PRIVATE)
         sharedPreferences.edit().putBoolean("enabled_by_admin", true).apply()*/
 
-        val componentName = ComponentName("com.google.android.gms", "com.google.android.gms.auth.account.authenticator.WorkAccountAuthenticatorService")
-        context.packageManager.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP)
+        val componentName = ComponentName(
+            "com.google.android.gms",
+            "com.google.android.gms.auth.account.authenticator.WorkAccountAuthenticatorService"
+        )
+        packageManager.setComponentEnabledSetting(
+            componentName,
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            PackageManager.DONT_KILL_APP
+        )
     }
 
     override fun addWorkAccount(
         callback: IWorkAccountCallback?,
         token: String?
     ) {
-        // TODO: caller expects that an account is actually created
         Log.d(TAG, "addWorkAccount with token $token")
-        Log.d(TAG, "stub implementation, not creating account; please create manually!")
-        // if account already exists, return one of those as the "new one" (temporary implementation)
-        AccountManager.get(context).getAccountsByType("com.google.work").firstOrNull()?.let {
-            callback?.onAccountAdded(it)
+        val future = accountManager.addAccount(
+            WORK_ACCOUNT_TYPE,
+            null,
+            null,
+            Bundle().apply { putString(KEY_ACCOUNT_CREATION_TOKEN, token) },
+            null,
+            null,
+            null
+        )
+        callback?.let {
+            Thread {
+                future.result.let { result ->
+                    it.onAccountAdded(
+                        Account(
+                            result.getString(AccountManager.KEY_ACCOUNT_NAME),
+                            result.getString(AccountManager.KEY_ACCOUNT_TYPE)
+                        )
+                    )
+                }
+            }.start()
         }
     }
 
@@ -64,7 +100,7 @@ class WorkAccountServiceImpl(val context: Context) : IWorkAccountService.Stub() 
         // TODO: caller expects that the account is actually removed
         Log.d(TAG, "removeWorkAccount with account ${account?.name}")
         Log.d(TAG, "stub implementation, not removing account; please remove manually!")
-        val noAccounts = AccountManager.get(context).getAccountsByType("com.google.work").isEmpty()
+        val noAccounts = accountManager.getAccountsByType("com.google.work").isEmpty()
         callback?.onAccountRemoved(noAccounts)
 
     }
