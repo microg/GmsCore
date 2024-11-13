e052b4eb904162c7f2a3c692870ecc6a28210e22
davinci9196 <davinci9196@gmail.com>
2024-11-13 09:46:08 +0800

    Formatting Code

--- play-services-core/src/main/kotlin/org/microg/gms/auth/signin/AuthSignInActivity.kt
+++ play-services-core/src/main/kotlin/org/microg/gms/auth/signin/AuthSignInActivity.kt
@@ -16,7 +16,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
-import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.getSystemService
