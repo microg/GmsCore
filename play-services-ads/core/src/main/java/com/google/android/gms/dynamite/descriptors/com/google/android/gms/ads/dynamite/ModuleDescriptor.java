ec636fd758b314112647d421dbac5d776d382c8d
davinci9196 <davinci9196@gmail.com>
2024-12-05 21:58:20 +0800

    DynamiteModule: Added <init> method

--- play-services-ads/core/src/main/java/com/google/android/gms/dynamite/descriptors/com/google/android/gms/ads/dynamite/ModuleDescriptor.java
+++ play-services-ads/core/src/main/java/com/google/android/gms/dynamite/descriptors/com/google/android/gms/ads/dynamite/ModuleDescriptor.java
@@ -5,10 +5,20 @@
 
 package com.google.android.gms.dynamite.descriptors.com.google.android.gms.ads.dynamite;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.util.Log;
+import android.webkit.WebSettings;
 import androidx.annotation.Keep;
 
 @Keep
 public class ModuleDescriptor {
     public static final String MODULE_ID = "com.google.android.gms.ads.dynamite";
     public static final int MODULE_VERSION = 230500001;
+
+    public static void init(ContextWrapper context) {
+        Context baseContext = context.getBaseContext();
+        WebSettings.getDefaultUserAgent(baseContext);
+        Log.d("ModuleDescriptor", "init: context: " + baseContext);
+    }
 }
