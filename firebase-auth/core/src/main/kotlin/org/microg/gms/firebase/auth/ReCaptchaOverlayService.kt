768526c90baad208e80c4eb4f65fdbaea2a20250
davinci9196 <davinci9196@gmail.com>
2024-12-13 22:22:53 +0800

    change to service

--- firebase-auth/core/src/main/kotlin/org/microg/gms/firebase/auth/ReCaptchaOverlayService.kt
+++ firebase-auth/core/src/main/kotlin/org/microg/gms/firebase/auth/ReCaptchaOverlayService.kt
@@ -7,13 +7,14 @@ package org.microg.gms.firebase.auth
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.Service
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.content.Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
-import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
-import android.content.Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+import android.content.ServiceConnection
 import android.graphics.PixelFormat
 import android.os.Bundle
+import android.os.IBinder
 import android.os.ResultReceiver
 import android.provider.Settings
 import android.util.DisplayMetrics
@@ -23,7 +24,6 @@ import android.webkit.JavascriptInterface
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.widget.FrameLayout
-import androidx.appcompat.app.AppCompatActivity
 import org.microg.gms.firebase.auth.core.R
 import org.microg.gms.profile.Build
 import org.microg.gms.profile.ProfileManager
@@ -33,24 +33,36 @@ import kotlin.coroutines.suspendCoroutine
 
 private const val TAG = "GmsFirebaseAuthCaptcha"
 
-class ReCaptchaOverlayActivity : AppCompatActivity() {
+class ReCaptchaOverlayService : Service() {
 
-    private val receiver: ResultReceiver?
-        get() = intent.getParcelableExtra(EXTRA_RESULT_RECEIVER)
-    private val hostname: String
-        get() = intent.getStringExtra(EXTRA_HOSTNAME) ?: "localhost:5000"
+    private var receiver: ResultReceiver? = null
+    private var hostname: String? = null
+    private var apiKey: String? = null
+
     private var finished = false
     private var container: View? = null
+    private var windowManager: WindowManager? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onBind(intent: Intent): IBinder? {
+        init(intent)
+        return null
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        finishResult(Activity.RESULT_CANCELED)
+        return super.onUnbind(intent)
+    }
+
+    private fun init(intent: Intent) {
+        apiKey = intent.getStringExtra(EXTRA_API_KEY) ?: return finishResult(Activity.RESULT_CANCELED)
+        receiver = intent.getParcelableExtra(EXTRA_RESULT_RECEIVER)
+        hostname = intent.getStringExtra(EXTRA_HOSTNAME) ?: "localhost:5000"
+        windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
         show()
     }
 
     @SuppressLint("SetJavaScriptEnabled", "AddJavascriptInterface")
     private fun show() {
-        val apiKey = intent.getStringExtra(EXTRA_API_KEY) ?: return cancel()
-        val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
         val layoutParamsType = if (android.os.Build.VERSION.SDK_INT >= 26) {
             WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
         } else {
@@ -72,7 +84,7 @@ class ReCaptchaOverlayActivity : AppCompatActivity() {
             override fun dispatchKeyEvent(event: KeyEvent): Boolean {
                 if (event.action == KeyEvent.ACTION_DOWN) {
                     if (event.keyCode == KeyEvent.KEYCODE_BACK || event.keyCode == KeyEvent.KEYCODE_HOME) {
-                        cancel()
+                        finishResult(Activity.RESULT_CANCELED)
                         return true
                     }
                 }
@@ -88,7 +100,7 @@ class ReCaptchaOverlayActivity : AppCompatActivity() {
             val pad = (5.0 * (resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)).toInt()
             container.setOnTouchListener { v, _ ->
                 v.performClick()
-                cancel()
+                finishResult(Activity.RESULT_CANCELED)
                 return@setOnTouchListener true
             }
             val view = container.findViewById<WebView>(R.id.web)
@@ -102,55 +114,47 @@ class ReCaptchaOverlayActivity : AppCompatActivity() {
             ProfileManager.ensureInitialized(this)
             settings.userAgentString = Build.generateWebViewUserAgentString(settings.userAgentString)
             view.addJavascriptInterface(ReCaptchaCallback(this), "MyCallback")
-            val captcha = assets.open("recaptcha.html").bufferedReader().readText().replace("%apikey%", apiKey)
+            val captcha = assets.open("recaptcha.html").bufferedReader().readText().replace("%apikey%", apiKey!!)
             view.loadDataWithBaseURL("https://$hostname/", captcha, null, null, "https://$hostname/")
-            windowManager.addView(container, params)
+            windowManager?.addView(container, params)
         }
-    }
-
-    fun cancel() {
-        if (!finished) {
-            finished = true
-            finishResult(Activity.RESULT_CANCELED)
-        }
-        close()
-    }
-
-    fun close() {
-        container?.let { windowManager.removeView(it) }
     }
 
     fun finishResult(resultCode: Int, token: String? = null) {
-        finished = true
-        setResult(resultCode, token?.let { Intent().apply { putExtra(EXTRA_TOKEN, it) } })
-        receiver?.send(resultCode, token?.let { Bundle().apply { putString(EXTRA_TOKEN, it) } })
-        finish()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        cancel()
+        if (!finished) {
+            finished = true
+            receiver?.send(resultCode, token?.let { Bundle().apply { putString(EXTRA_TOKEN, it) } })
+        }
+        container?.let { windowManager?.removeView(it) }
     }
 
     companion object {
-        class ReCaptchaCallback(private val overlay: ReCaptchaOverlayActivity) {
+
+        private val recaptchaServiceConnection = object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                Log.d(TAG, "onReCaptchaToken: onServiceConnected: $name")
+            }
+
+            override fun onServiceDisconnected(name: ComponentName?) {
+                Log.d(TAG, "onReCaptchaToken: onServiceDisconnected: $name")
+            }
+        }
+
+        class ReCaptchaCallback(private val overlay: ReCaptchaOverlayService) {
             @JavascriptInterface
             fun onReCaptchaToken(token: String) {
                 Log.d(TAG, "onReCaptchaToken: $token")
-                if (!overlay.finished) {
-                    overlay.finished = true
-                    overlay.finishResult(Activity.RESULT_OK, token)
-                }
-                overlay.close()
+                overlay.finishResult(Activity.RESULT_OK, token)
             }
         }
 
         fun isSupported(context: Context): Boolean = android.os.Build.VERSION.SDK_INT < 23 || Settings.canDrawOverlays(context)
 
         suspend fun awaitToken(context: Context, apiKey: String, hostname: String? = null) = suspendCoroutine { continuation ->
-            val intent = Intent(context, ReCaptchaOverlayActivity::class.java)
+            val intent = Intent(context, ReCaptchaOverlayService::class.java)
             val resultReceiver = object : ResultReceiver(null) {
                 override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
+                    context.unbindService(recaptchaServiceConnection)
                     try {
                         if (resultCode == Activity.RESULT_OK) {
                             continuation.resume(resultData?.getString(EXTRA_TOKEN)!!)
@@ -163,10 +167,7 @@ class ReCaptchaOverlayActivity : AppCompatActivity() {
             intent.putExtra(EXTRA_API_KEY, apiKey)
             intent.putExtra(EXTRA_RESULT_RECEIVER, resultReceiver)
             intent.putExtra(EXTRA_HOSTNAME, hostname)
-            intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
-            intent.addFlags(FLAG_ACTIVITY_REORDER_TO_FRONT)
-            intent.addFlags(FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
-            context.startActivity(intent)
+            context.bindService(intent, recaptchaServiceConnection, BIND_AUTO_CREATE)
         }
     }
 }
