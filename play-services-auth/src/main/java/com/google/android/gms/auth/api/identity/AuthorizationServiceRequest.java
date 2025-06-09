339bc42d5d39191ebcd35ee93e1d61cf7d58498a
Tripps20 <ekanemwilfred75@gmail.com>
2025-06-09 14:01:29 +0100

    Auth: Add AuthorizationServiceRequest class with builder pattern and parcelable support for Auth Identity API

---
+++ play-services-auth/src/main/java/com/google/android/gms/auth/api/identity/AuthorizationServiceRequest.java
@@ -0,0 +1,87 @@
+package com.google.android.gms.auth.api.identity;
+
+import com.google.android.gms.common.internal.safeparcel.AbstractSafeParcelable;
+import com.google.android.gms.common.internal.safeparcel.SafeParcelable;
+import com.google.android.gms.common.internal.safeparcel.SafeParcelable.Class;
+import com.google.android.gms.common.internal.safeparcel.SafeParcelable.Field;
+
+import androidx.annotation.Nullable;
+
+@Class(creator = "AuthorizationServiceRequestCreator")
+public class AuthorizationServiceRequest extends AbstractSafeParcelable {
+
+    @Field(id = 1)
+    @Nullable
+    public final String tokenType;
+
+    @Field(id = 2)
+    @Nullable
+    public final String clientId;
+
+    @Field(id = 3)
+    @Nullable
+    public final String scope;
+
+    @Field(id = 4)
+    @Nullable
+    public final String redirectUri;
+
+    public static final Creator<AuthorizationServiceRequest> CREATOR =
+            new AutoSafeParcelable.Creator<>(AuthorizationServiceRequest.class);
+
+    private AuthorizationServiceRequest(
+            @Nullable String tokenType,
+            @Nullable String clientId,
+            @Nullable String scope,
+            @Nullable String redirectUri) {
+        this.tokenType = tokenType;
+        this.clientId = clientId;
+        this.scope = scope;
+        this.redirectUri = redirectUri;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorizationServiceRequest{" +
+                "tokenType='" + tokenType + '\'' +
+                ", clientId='" + clientId + '\'' +
+                ", scope='" + scope + '\'' +
+                ", redirectUri='" + redirectUri + '\'' +
+                '}';
+    }
+
+    public static final class Builder {
+        private String tokenType;
+        private String clientId;
+        private String scope;
+        private String redirectUri;
+
+        public Builder setTokenType(String tokenType) {
+            this.tokenType = tokenType;
+            return this;
+        }
+
+        public Builder setClientId(String clientId) {
+            this.clientId = clientId;
+            return this;
+        }
+
+        public Builder setScope(String scope) {
+            this.scope = scope;
+            return this;
+        }
+
+        public Builder setRedirectUri(String redirectUri) {
+            this.redirectUri = redirectUri;
+            return this;
+        }
+
+        public AuthorizationServiceRequest build() {
+            return new AuthorizationServiceRequest(tokenType, clientId, scope, redirectUri);
+        }
+    }
+  }
