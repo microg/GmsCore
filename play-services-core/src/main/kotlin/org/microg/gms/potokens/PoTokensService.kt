b58c6e48cd89de90d244b7c877d222015526e93b
DaVinci9196 <150454414+DaVinci9196@users.noreply.github.com>
2025-07-11 18:52:52 +0800

    PoToken: Optimize po-token refresh logic (#2894)

--- play-services-core/src/main/kotlin/org/microg/gms/potokens/PoTokensService.kt
+++ play-services-core/src/main/kotlin/org/microg/gms/potokens/PoTokensService.kt
@@ -5,9 +5,9 @@
 
 package org.microg.gms.potokens
 
-import android.content.Context
 import android.util.Log
-import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.common.Feature
 import com.google.android.gms.common.api.CommonStatusCodes
@@ -23,17 +23,16 @@ import org.microg.gms.BaseService
 import org.microg.gms.common.GmsService
 import org.microg.gms.profile.ProfileManager
 
-const val TAG = "PoTokens"
 private val FEATURES = arrayOf(Feature("PO_TOKENS", 1))
 
 class PoTokensService : BaseService(TAG, GmsService.POTOKENS) {
+
     override fun handleServiceRequest(callback: IGmsCallbacks, request: GetServiceRequest, service: GmsService) {
         Log.d(TAG, "PoTokensApiService handleServiceRequest")
+        ProfileManager.ensureInitialized(this)
         callback.onPostInitCompleteWithConnectionInfo(
             CommonStatusCodes.SUCCESS,
-            PoTokensServiceImpl(
-                applicationContext, request.packageName, lifecycleScope
-            ),
+            PoTokensServiceImpl(request.packageName, PoTokenHelper(this), lifecycle),
             ConnectionInfo().apply { features = FEATURES }
         )
     }
@@ -41,10 +40,10 @@ class PoTokensService : BaseService(TAG, GmsService.POTOKENS) {
 
 
 class PoTokensServiceImpl(
-    private val context: Context,
     private val packageName: String,
-    private val lifecycleCoroutineScope: LifecycleCoroutineScope
-) : IPoTokensService.Stub() {
+    private val helper: PoTokenHelper,
+    override val lifecycle: Lifecycle
+) : IPoTokensService.Stub(), LifecycleOwner {
 
     override fun responseStatus(call: IStatusCallback, code: Int) {
         Log.e(TAG, "responseStatus success")
@@ -53,12 +52,10 @@ class PoTokensServiceImpl(
 
     override fun responseStatusToken(call: ITokenCallbacks, i: Int, bArr: ByteArray) {
         Log.d(TAG, "responseStatusToken packageName: $packageName")
-        ProfileManager.ensureInitialized(context)
-
-        lifecycleCoroutineScope.launchWhenCreated {
+        lifecycleScope.launchWhenStarted {
             Log.d(TAG, "responseStatusToken start")
             runCatching {
-                val bytes = PoTokenHelper.get(context).callPoToken(context, packageName, bArr)
+                val bytes = helper.callPoToken(packageName, bArr)
                 Log.d(TAG, "responseStatusToken result: ${bytes.size}")
                 call.responseToken(Status.SUCCESS, PoToken(bytes))
             }
