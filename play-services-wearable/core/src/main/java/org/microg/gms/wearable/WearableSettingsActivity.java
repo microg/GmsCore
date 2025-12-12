b1d1aaa031facf4eeb4b0560c405a957c965f95e
SBALAVIGNESH123 <your.email@example.com>
2025-12-12 20:49:55 +0530

    Fix compilation error: remove duplicate closing brace

--- play-services-wearable/core/src/main/java/org/microg/gms/wearable/WearableSettingsActivity.java
+++ play-services-wearable/core/src/main/java/org/microg/gms/wearable/WearableSettingsActivity.java
@@ -101,7 +101,6 @@ public class WearableSettingsActivity extends Activity {
             builder.setNegativeButton("Cancel", null);
             builder.show();
         });
-        });
     }
 
     @Override
