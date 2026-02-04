43fc555fa135a9ccf90fc0c6663e3bd1347b7a33
DaVinci9196 <davinci9196@gmail.com>
2026-02-04 18:58:37 +0800

    Revert "1.Avoiding ANR that may occur during login"
    This reverts commit 271cdd25a22bb770edfa48af0807f8cec19a3eb4.

--- play-services-core/src/main/kotlin/org/microg/gms/auth/login/DroidGuardHandler.kt
+++ play-services-core/src/main/kotlin/org/microg/gms/auth/login/DroidGuardHandler.kt
@@ -5,52 +5,20 @@
 
 package org.microg.gms.auth.login
 
-import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.droidguard.DroidGuardClient
-import com.google.android.gms.tasks.Tasks
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.util.Collections
-import java.util.concurrent.TimeUnit
-
-private const val TAG = "DroidGuardHandler"
-
-interface DroidGuardCallback {
-    fun onSuccess(result: String)
-    fun onError(error: Throwable)
-}
+import com.google.android.gms.tasks.await
+import java.util.*
 
 class DroidGuardHandler(private val activity: LoginActivity) {
     fun start(dg: String) {
-        getDroidGuardResultAsync("minute_maid", Collections.singletonMap("dg_minutemaid", dg), object : DroidGuardCallback {
-            override fun onSuccess(result: String) {
-                Log.d(TAG, "start: result: $result")
-                activity.runScript("window.setDgResult('$result')")
-            }
-
-            override fun onError(error: Throwable) {
-                Log.w(TAG, "onError: ", error)
-            }})
-    }
-
-    fun getDroidGuardResultAsync(flow: String, data: Map<String, String>, callback: DroidGuardCallback) {
         activity.lifecycleScope.launchWhenStarted {
-            val start = System.currentTimeMillis()
             try {
-                val result = withContext(Dispatchers.IO) {
-                    val resultTask = DroidGuardClient.getResults(activity, flow, data)
-                    Tasks.await(resultTask, 5000, TimeUnit.MILLISECONDS)
-                }
-                Log.d(TAG, "getDroidGuardResultAsync flow:$flow result:$result")
-                withContext(Dispatchers.Main) {
-                    callback.onSuccess(result)
-                }
+                val result = DroidGuardClient.getResults(activity, "minute_maid", Collections.singletonMap("dg_minutemaid", dg)).await()
+                activity.runScript("window.setDgResult('$result')")
             } catch (e: Exception) {
-                callback.onError(e)
-                Log.w(TAG, "getDroidGuardResultAsync : ", e)
+                // Ignore
             }
-            Log.d(TAG, "getDroidGuardResultAsync end " + (System.currentTimeMillis() - start))
         }
     }
 }
