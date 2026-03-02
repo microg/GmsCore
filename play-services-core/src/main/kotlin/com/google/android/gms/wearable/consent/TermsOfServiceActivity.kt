ba8b3aac716d84f0cd8e7320134e59fac921e57b
Akinniranye Samuel Tomiwa <benneu40@gmail.com>
2026-03-02 19:50:23 +0100

    Gate Wearable TOS auto-accept behind an opt-in user setting

--- play-services-core/src/main/kotlin/com/google/android/gms/wearable/consent/TermsOfServiceActivity.kt
+++ play-services-core/src/main/kotlin/com/google/android/gms/wearable/consent/TermsOfServiceActivity.kt
@@ -7,12 +7,17 @@ package com.google.android.gms.wearable.consent
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.microg.gms.wearable.WearablePreferences
 
 class TermsOfServiceActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setResult(RESULT_OK)
+        if (WearablePreferences.isAutoAcceptTosEnabled(this)) {
+            setResult(RESULT_OK)
+        } else {
+            setResult(RESULT_CANCELED)
+        }
         finish()
     }
 }
