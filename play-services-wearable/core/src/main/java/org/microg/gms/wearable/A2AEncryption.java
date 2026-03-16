61a6ed24c8a7240f156de3cbb43d1525297f5674
deadYokai <mynameiskitsune@proton.me>
2026-03-16 18:17:24 +0200

    a2a encryption stub draft

---
+++ play-services-wearable/core/src/main/java/org/microg/gms/wearable/A2AEncryption.java
@@ -0,0 +1,10 @@
+package org.microg.gms.wearable;
+
+public class A2AEncryption {
+    private static final String TAG = "WearA2AEncryption";
+
+    public static final byte[] CONFIRM = {67, 79, 78, 70, 73, 82, 77};
+    public static final byte[] CLIENT = {67, 76, 73, 69, 78, 84};
+    public static final byte[] SERVER = {83, 69, 82, 86, 69, 82};
+    private static final byte[] RESUME = {82, 69, 83, 85, 77, 69};
+}
