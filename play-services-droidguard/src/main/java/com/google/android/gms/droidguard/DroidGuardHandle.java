9f9ba680f0588c94847036eabbf476c0aff971bd
Snooz1e <rikorikovych@gmail.com>
2026-05-13 19:56:19 +0300

    Add multi-step remote DroidGuard session support (#2851)

--- play-services-droidguard/src/main/java/com/google/android/gms/droidguard/DroidGuardHandle.java
+++ play-services-droidguard/src/main/java/com/google/android/gms/droidguard/DroidGuardHandle.java
@@ -5,10 +5,21 @@
 
 package com.google.android.gms.droidguard;
 
+import com.google.android.gms.droidguard.internal.DroidGuardInitReply;
+import com.google.android.gms.droidguard.internal.DroidGuardResultsRequest;
+
 import java.util.Map;
 
 public interface DroidGuardHandle {
     String snapshot(Map<String, String> data);
+
+    long begin(String flow, DroidGuardResultsRequest request, Map<String, String> initialData);
+
+    DroidGuardInitReply nextStep(long sessionId, Map<String, String> stepData);
+
+    String snapshotWithSession(long sessionId, Map<String, String> data);
+
+    void closeSession(long sessionId);
 
     boolean isOpened();
 
