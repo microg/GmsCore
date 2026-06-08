046c00b0d160f7a355c718acc9ee595ccefe2948
DaVinci9196 <150454414+DaVinci9196@users.noreply.github.com>
2026-06-09 04:25:34 +0800

    Phenotype: Fixed the issue of no Gemini entry point in Google Messages. (#3380)
    Co-authored-by: Marvin W <git@larma.de>

--- play-services-core/src/main/kotlin/org/microg/gms/phenotype/PhenotypeService.kt
+++ play-services-core/src/main/kotlin/org/microg/gms/phenotype/PhenotypeService.kt
@@ -102,6 +102,10 @@ private val CONFIGURATION_OPTIONS = mapOf(
         // Enable NotebookLM video overview
         Flag("45741830", true, 0),
     ),
+    "com.google.android.apps.messaging#com.google.android.apps.messaging" to arrayOf(
+        Flag("bugle_phenotype__enable_penpal_conversation", true, 0),
+        Flag("bugle_phenotype__bug_325090692_enable_penpal_dasher_check", false, 0),
+    ),
 )
 
 class PhenotypeServiceImpl(val packageName: String?) : IPhenotypeService.Stub() {
