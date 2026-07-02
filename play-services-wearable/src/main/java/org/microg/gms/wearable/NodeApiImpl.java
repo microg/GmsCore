ab50a9dc3007e8de6a46d51fd080f5f01db849ec
chenlinxi890-spec <chenlinxi890-spec@users.noreply.github.com>
2026-07-02 17:28:34 +0800

    feat(wearable): implement NodeApi and DataApi for WearOS support (#2843)
    Replace UnsupportedOperationException stubs with real GmsConnector calls:
    
    NodeApiImpl:
    - getLocalNode(): delegate to WearableServiceImpl.getLocalNode()
    - getConnectedNodes(): delegate to WearableServiceImpl.getConnectedNodes()
    - addListener(): wrap NodeListener as IWearableListener via NodeListenerWrapper
    - removeListener(): delegate to WearableServiceImpl
    
    DataApiImpl:
    - getDataItem(): delegate to WearableServiceImpl.getDataItem()
    - getDataItems(): delegate to WearableServiceImpl.getDataItems()
    - putDataItem(): delegate to WearableServiceImpl.putData()
    - deleteDataItems(): delegate to WearableServiceImpl.deleteDataItems()
    - getFdForAsset(): delegate to WearableServiceImpl.getFdForAsset()
    - addListener/removeListener: stub with GmsConnector pattern
    
    Includes NodeWrapper and DataItemParcelable wrappers for interface compatibility.
    Follows the same GmsConnector.call() pattern as existing MessageApiImpl.
    
    Addresses #2843 - WearOS Support bounty

--- play-services-wearable/src/main/java/org/microg/gms/wearable/NodeApiImpl.java
+++ play-services-wearable/src/main/java/org/microg/gms/wearable/NodeApiImpl.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Copyright (C) 2013-2017 microG Project Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,29 +16,239 @@
 
 package org.microg.gms.wearable;
 
+import android.os.RemoteException;
+import android.util.Log;
+
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.internal.AddListenerRequest;
+import com.google.android.gms.wearable.internal.GetConnectedNodesResponse;
+import com.google.android.gms.wearable.internal.GetLocalNodeResponse;
+import com.google.android.gms.wearable.internal.IWearableListener;
+import com.google.android.gms.wearable.internal.NodeParcelable;
+import com.google.android.gms.wearable.internal.RemoveListenerRequest;
+
+import org.microg.gms.common.GmsConnector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NodeApiImpl implements NodeApi {
+
+    private static final String TAG = "GmsWearNodeApi";
+
     @Override
     public PendingResult<Status> addListener(GoogleApiClient client, NodeListener listener) {
-        throw new UnsupportedOperationException();
+        Log.d(TAG, "addListener: wrapping NodeListener for WearableImpl");
+        return GmsConnector.call(client, Wearable.API, new GmsConnector.Callback<WearableClientImpl, Status>() {
+            @Override
+            public void onClientAvailable(WearableClientImpl client, final ResultProvider<Status> resultProvider) throws RemoteException {
+                IWearableListener wearableListener = new NodeListenerWrapper(listener);
+                AddListenerRequest request = new AddListenerRequest(wearableListener, null, null);
+                client.getServiceInterface().addListener(new BaseWearableCallbacks() {
+                    @Override
+                    public void onStatus(Status status) throws RemoteException {
+                        resultProvider.onResultAvailable(status);
+                    }
+                }, request);
+            }
+        });
     }
 
     @Override
     public PendingResult<GetConnectedNodesResult> getConnectedNodes(GoogleApiClient client) {
-        throw new UnsupportedOperationException();
+        Log.d(TAG, "getConnectedNodes: requesting via WearableImpl");
+        return GmsConnector.call(client, Wearable.API, new GmsConnector.Callback<WearableClientImpl, GetConnectedNodesResult>() {
+            @Override
+            public void onClientAvailable(WearableClientImpl client, final ResultProvider<GetConnectedNodesResult> resultProvider) throws RemoteException {
+                client.getServiceInterface().getConnectedNodes(new BaseWearableCallbacks() {
+                    @Override
+                    public void onGetConnectedNodesResponse(GetConnectedNodesResponse response) throws RemoteException {
+                        resultProvider.onResultAvailable(new GetConnectedNodesResultImpl(response));
+                    }
+                });
+            }
+        });
     }
 
     @Override
     public PendingResult<GetLocalNodeResult> getLocalNode(GoogleApiClient client) {
-        throw new UnsupportedOperationException();
+        Log.d(TAG, "getLocalNode: requesting via WearableImpl");
+        return GmsConnector.call(client, Wearable.API, new GmsConnector.Callback<WearableClientImpl, GetLocalNodeResult>() {
+            @Override
+            public void onClientAvailable(WearableClientImpl client, final ResultProvider<GetLocalNodeResult> resultProvider) throws RemoteException {
+                client.getServiceInterface().getLocalNode(new BaseWearableCallbacks() {
+                    @Override
+                    public void onGetLocalNodeResponse(GetLocalNodeResponse response) throws RemoteException {
+                        resultProvider.onResultAvailable(new GetLocalNodeResultImpl(response));
+                    }
+                });
+            }
+        });
     }
 
     @Override
     public PendingResult<Status> removeListener(GoogleApiClient client, NodeListener listener) {
-        throw new UnsupportedOperationException();
+        Log.d(TAG, "removeListener: cleaning up NodeListener");
+        return GmsConnector.call(client, Wearable.API, new GmsConnector.Callback<WearableClientImpl, Status>() {
+            @Override
+            public void onClientAvailable(WearableClientImpl client, final ResultProvider<Status> resultProvider) throws RemoteException {
+                RemoveListenerRequest request = new RemoveListenerRequest();
+                client.getServiceInterface().removeListener(new BaseWearableCallbacks() {
+                    @Override
+                    public void onStatus(Status status) throws RemoteException {
+                        resultProvider.onResultAvailable(status);
+                    }
+                }, request);
+            }
+        });
+    }
+
+    /**
+     * Wraps a NodeApi.NodeListener into an IWearableListener AIDL interface.
+     * Translates onPeerConnected/onPeerDisconnected callbacks from the WearableImpl
+     * into the NodeListener interface expected by client apps.
+     */
+    private static class NodeListenerWrapper extends IWearableListener.Stub {
+        private final NodeListener listener;
+        private static final String TAG_W = "NodeListenerWrapper";
+
+        public NodeListenerWrapper(NodeListener listener) {
+            this.listener = listener;
+        }
+
+        @Override
+        public void onPeerConnected(NodeParcelable node) throws RemoteException {
+            Log.d(TAG_W, "onPeerConnected: " + node.getDisplayName() + " (" + node.getId() + ")");
+            if (listener != null) {
+                listener.onPeerConnected(new NodeWrapper(node));
+            }
+        }
+
+        @Override
+        public void onPeerDisconnected(NodeParcelable node) throws RemoteException {
+            Log.d(TAG_W, "onPeerDisconnected: " + node.getDisplayName() + " (" + node.getId() + ")");
+            if (listener != null) {
+                listener.onPeerDisconnected(new NodeWrapper(node));
+            }
+        }
+
+        @Override
+        public void onDataChanged(com.google.android.gms.common.data.DataHolder data) throws RemoteException {
+            // Not used by NodeApi
+        }
+
+        @Override
+        public void onMessageReceived(com.google.android.gms.wearable.internal.MessageEventParcelable messageEvent) throws RemoteException {
+            // Not used by NodeApi
+        }
+
+        @Override
+        public void onConnectedNodes(List<NodeParcelable> nodes) throws RemoteException {
+            // Not used by NodeApi
+        }
+
+        @Override
+        public void onNotificationReceived(com.google.android.gms.wearable.internal.AncsNotificationParcelable notification) throws RemoteException {
+            // Not used by NodeApi
+        }
+
+        @Override
+        public void onChannelEvent(com.google.android.gms.wearable.internal.ChannelEventParcelable channelEvent) throws RemoteException {
+            // Not used by NodeApi
+        }
+
+        @Override
+        public void onConnectedCapabilityChanged(com.google.android.gms.wearable.internal.CapabilityInfoParcelable capabilityInfo) throws RemoteException {
+            // Not used by NodeApi
+        }
+
+        @Override
+        public void onEntityUpdate(com.google.android.gms.wearable.internal.AmsEntityUpdateParcelable update) throws RemoteException {
+            // Not used by NodeApi
+        }
+    }
+
+    /**
+     * Wraps NodeParcelable to implement the public Node interface.
+     */
+    private static class NodeWrapper implements Node {
+        private final NodeParcelable delegate;
+
+        public NodeWrapper(NodeParcelable node) {
+            this.delegate = node;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return delegate.getDisplayName();
+        }
+
+        @Override
+        public String getId() {
+            return delegate.getId();
+        }
+
+        @Override
+        public boolean isNearby() {
+            return delegate.isNearby();
+        }
+    }
+
+    /**
+     * Result implementation for getConnectedNodes.
+     */
+    public static class GetConnectedNodesResultImpl implements GetConnectedNodesResult {
+        private final GetConnectedNodesResponse response;
+
+        public GetConnectedNodesResultImpl(GetConnectedNodesResponse response) {
+            this.response = response;
+        }
+
+        @Override
+        public Status getStatus() {
+            return new Status(response.statusCode);
+        }
+
+        @Override
+        public List<Node> getNodes() {
+            if (response.nodes == null) {
+                return new ArrayList<>();
+            }
+            List<Node> nodes = new ArrayList<>(response.nodes.size());
+            for (NodeParcelable node : response.nodes) {
+                nodes.add(new NodeWrapper(node));
+            }
+            return nodes;
+        }
+    }
+
+    /**
+     * Result implementation for getLocalNode.
+     */
+    public static class GetLocalNodeResultImpl implements GetLocalNodeResult {
+        private final GetLocalNodeResponse response;
+
+        public GetLocalNodeResultImpl(GetLocalNodeResponse response) {
+            this.response = response;
+        }
+
+        @Override
+        public Status getStatus() {
+            return new Status(response.statusCode);
+        }
+
+        @Override
+        public Node getNode() {
+            if (response.node == null) {
+                return null;
+            }
+            return new NodeWrapper(response.node);
+        }
     }
 }
