9315a707ff0a621e3a2190ceeb7a7b5d62b6c80d
Rushi Ranpise <rushiranpise17@gmail.com>
2026-08-13 23:20:34 -0700

    feat(ui): mirror MicroG-RE foreground notification and self-check

--- play-services-base/core/src/main/java/org/microg/gms/common/ForegroundServiceContext.java
+++ play-services-base/core/src/main/java/org/microg/gms/common/ForegroundServiceContext.java
@@ -1,14 +1,18 @@
 package org.microg.gms.common;
 
 import android.app.Notification;
+import android.app.Notification.Action;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
+import android.graphics.drawable.Icon;
 import android.os.PowerManager;
+import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.RequiresApi;
@@ -84,22 +88,67 @@ public class ForegroundServiceContext extends ContextWrapper {
 
     @RequiresApi(26)
     private static Notification buildForegroundNotification(Context context, String serviceName) {
-        NotificationChannel channel = new NotificationChannel("foreground-service", "Foreground Service", NotificationManager.IMPORTANCE_NONE);
+        // Notification channel (high importance, mirrors MicroG-RE)
+        String channelName = context.getString(R.string.foreground_service_notification_title);
+        NotificationChannel channel = new NotificationChannel("foreground-service", channelName, NotificationManager.IMPORTANCE_HIGH);
         channel.setLockscreenVisibility(Notification.VISIBILITY_SECRET);
         channel.setShowBadge(false);
-        channel.setVibrationPattern(new long[]{0});
         context.getSystemService(NotificationManager.class).createNotificationChannel(channel);
+
+        // Title and text
         String appTitle = context.getApplicationInfo().loadLabel(context.getPackageManager()).toString();
         String notifyTitle = context.getString(R.string.foreground_service_notification_title);
         String firstLine = context.getString(R.string.foreground_service_notification_text, serviceName);
         String secondLine = context.getString(R.string.foreground_service_notification_big_text, appTitle);
+
+        // Open battery optimizations settings
+        Intent batteryOptimizationIntent = ForegroundServiceOemUtils.getBatteryOptimizationIntent(context);
+        PendingIntent batteryPendingIntent = PendingIntent.getActivity(
+                context, 0, batteryOptimizationIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
+
+        // Open notification settings in foreground service category
+        Intent notificationCategoryIntent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS)
+                .putExtra(Settings.EXTRA_APP_PACKAGE, context.getPackageName())
+                .putExtra(Settings.EXTRA_CHANNEL_ID, "foreground-service");
+        PendingIntent notificationCategoryPendingIntent = PendingIntent.getActivity(
+                context, 1, notificationCategoryIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
+
+        // Open settings activity when notification is tapped
+        Intent mainSettingsIntent = new Intent();
+        mainSettingsIntent.setClassName(context.getPackageName(), "org.microg.gms.ui.MainSettingsActivity");
+        mainSettingsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent mainSettingsPendingIntent = PendingIntent.getActivity(
+                context, 2, mainSettingsIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
+
+        // Notification actions
+        Action batteryAction = new Action.Builder(
+                Icon.createWithResource(context, R.drawable.ic_battery_action),
+                context.getString(R.string.foreground_action_battery_optimization),
+                batteryPendingIntent
+        ).build();
+
+        Action notificationAction = new Action.Builder(
+                Icon.createWithResource(context, R.drawable.ic_notification_action),
+                context.getString(R.string.foreground_action_notification_settings),
+                notificationCategoryPendingIntent
+        ).build();
+
         Log.d(TAG, notifyTitle + " // " + firstLine + " // " + secondLine);
+
         return new Notification.Builder(context, channel.getId())
                 .setOngoing(true)
                 .setSmallIcon(R.drawable.ic_background_notify)
                 .setContentTitle(notifyTitle)
                 .setContentText(firstLine)
                 .setStyle(new Notification.BigTextStyle().bigText(firstLine + "\n" + secondLine))
+                .setPriority(Notification.PRIORITY_HIGH)
+                .setShowWhen(false)
+                .setContentIntent(mainSettingsPendingIntent)
+                .addAction(batteryAction)
+                .addAction(notificationAction)
                 .build();
     }
 
--- play-services-core/src/main/java/org/microg/tools/selfcheck/InstalledPackagesChecks.java
+++ play-services-core/src/main/java/org/microg/tools/selfcheck/InstalledPackagesChecks.java
@@ -19,6 +19,7 @@ package org.microg.tools.selfcheck;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.util.Log;
 
@@ -28,6 +29,8 @@ import com.google.android.gms.R;
 
 import org.microg.gms.common.Constants;
 import org.microg.gms.common.PackageUtils;
+
+import java.util.List;
 
 import static org.microg.tools.selfcheck.SelfCheckGroup.Result.Negative;
 import static org.microg.tools.selfcheck.SelfCheckGroup.Result.Positive;
@@ -44,6 +47,31 @@ public class InstalledPackagesChecks implements SelfCheckGroup {
         addPackageInstalledAndSignedResult(context, collector, context.getString(R.string.self_check_pkg_gms), Constants.GMS_PACKAGE_NAME, Constants.GMS_PACKAGE_SIGNATURE_SHA1);
         addPackageInstalledAndSignedResult(context, collector, context.getString(R.string.self_check_pkg_vending), Constants.VENDING_PACKAGE_NAME, Constants.GMS_PACKAGE_SIGNATURE_SHA1);
         addPackageInstalledResult(context, collector, context.getString(R.string.self_check_pkg_gsf), Constants.GSF_PACKAGE_NAME);
+        // Re-signed Google app forks (MicroG-RE parity)
+        checkInstalledPackage(context, collector, context.getString(R.string.about_morphe), ".morphe.android");
+        checkInstalledPackage(context, collector, context.getString(R.string.revanced), ".revanced.android");
+        checkInstalledPackage(context, collector, context.getString(R.string.revanced_extended), ".rvx.android");
+        checkInstalledPackage(context, collector, context.getString(R.string.youtube_advanced), ".rex.android");
+        checkInstalledPackage(context, collector, context.getString(R.string.vanced), ".vanced.android");
+    }
+
+    private void checkInstalledPackage(Context context, ResultCollector collector, String nicePackageName, String packageNameSubstring) {
+        boolean packageExists = isPackageInstalled(context, packageNameSubstring);
+        collector.addResult(context.getString(R.string.self_check_patched_app_installed, nicePackageName),
+                packageExists ? Positive : Negative,
+                context.getString(R.string.self_check_resolution_patched_app_installed, nicePackageName));
+    }
+
+    private boolean isPackageInstalled(Context context, String packageNameSubstring) {
+        PackageManager pm = context.getPackageManager();
+        List<PackageInfo> packages = pm.getInstalledPackages(0);
+        for (PackageInfo packageInfo : packages) {
+            String packageName = packageInfo.packageName;
+            if (packageName.contains(packageNameSubstring)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void addPackageInstalledAndSignedResult(Context context, ResultCollector collector, String nicePackageName, String androidPackageName, String signatureHash) {
