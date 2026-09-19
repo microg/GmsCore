4fb7583695a9c994e36bae4edc317f99f19c19dc
Fs00 <francescosaltori@gmail.com>
2026-09-19 17:31:52 +0200

    Tighten access to Google account info to match GMS behavior

--- play-services-core/src/main/java/org/microg/gms/people/PeopleServiceImpl.java
+++ play-services-core/src/main/java/org/microg/gms/people/PeopleServiceImpl.java
@@ -53,9 +53,7 @@ public class PeopleServiceImpl extends IPeopleService.Stub {
     @Override
     public void loadOwners(final IPeopleCallbacks callbacks, boolean var2, boolean var3, final String accountName, String var5, int sortOrder) {
         Log.d(TAG, "loadOwners: " + var2 + ", " + var3 + ", " + accountName + ", " + var5 + ", " + sortOrder);
-        if (context.checkCallingPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            PackageUtils.assertGooglePackagePermission(context, GooglePackagePermission.OWNER);
-        }
+        PackageUtils.assertGooglePackagePermission(context, GooglePackagePermission.OWNER);
         AccountManager accountManager = AccountManager.get(context);
         Bundle accountMetadata = new Bundle();
         String accountType = AuthConstants.DEFAULT_ACCOUNT_TYPE;
